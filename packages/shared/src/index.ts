ccbcef597983d4f0c53d5a0444c44bc60098ae4a
Netto Farias <35587885+Tokenizaa@users.noreply.github.com>
2026-09-09 14:05:01 -0300

    feat: export monthly plan planner

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,4 @@ export * from './planner/types.js';
 export { ProductScorer, ScoredProduct } from './planner/productScore.js';
 export * from './planner/antiRepetition.js';
 export * from './planner/categoryRotation.js';
+export * from './planner/monthlyPlan.js';

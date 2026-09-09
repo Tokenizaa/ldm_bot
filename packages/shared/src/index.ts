688f048814a76f41cd6c403e508f0c4e9ee5f07f
Netto Farias <35587885+Tokenizaa@users.noreply.github.com>
2026-09-09 14:59:18 -0300

    Fix shared ESM type exports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,7 +1,8 @@
 export * from './types.js';
 export * from './types/config.js';
 export * from './planner/types.js';
-export { ProductScorer, ScoredProduct } from './planner/productScore.js';
+export { ProductScorer } from './planner/productScore.js';
+export type { Product, ScoredProduct } from './planner/productScore.js';
 export * from './planner/antiRepetition.js';
 export * from './planner/categoryRotation.js';
 export * from './planner/monthlyPlan.js';
